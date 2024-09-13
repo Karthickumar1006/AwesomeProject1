8a16b132a7f0d7d1ae45e52b1e56f0494b1ad1c8
Karthic_Kumar_Elumalai <34277518+Karthickumar1006@users.noreply.github.com>
2024-09-13 12:37:57 +0530

    removing node modules

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default App;
