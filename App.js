fb6049ae23402fb3819ec14b43e28c919e8cc502
Karthic_Kumar_Elumalai <34277518+Karthickumar1006@users.noreply.github.com>
2024-10-14 10:08:01 +0530

    Adding plugin sample

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   SafeAreaView,
@@ -10,13 +10,47 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import { CustomText, CustomButton, CustomImage ,CustomInput,CustomRadioButton,CustomLoadingScreen} from '@karthickumarelumalai/rn-basic-setup';
 
+ 
 
 const App = () => {
+  const [inputText, setInputText] = useState('');
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const options = ["Option 1", "Option 2", "Option 3"];
+
+  const handleSelectOption = (option) => {
+      setSelectedOption(option);
+      console.log("Option",option);
+  };
+  const alertButton = () => {
+    alert('Button Clicked');
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <CustomText text="isConnected" style={{ color: 'red' }}></CustomText>
+      <CustomButton text="Click Me" onPress={() => alertButton()} />
+      <CustomImage sources={require("./assets/image.png")} style={{ width: 200, height:200 }}/>     
+      <CustomInput 
+        placeholder="Enter your name" 
+        value={inputText}
+        onChangeText={(text) => { setInputText(text); console.log("Text",text); }}
+        inputTextStyles = {{color: 'red'}} />
+        {options.map((option) => (
+                <CustomRadioButton
+                    key={option}
+                    label={option}
+                    selected={selectedOption === option}
+                    onPress={() => handleSelectOption(option)}
+                />
+            ))}
+
+     <CustomLoadingScreen color='red'/>
+
+    </View> 
   );
 };
 
